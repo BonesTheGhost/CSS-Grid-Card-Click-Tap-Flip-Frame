a9c698e65ec8dca2ce621bcb7d8b5abd83f9a2bd
Yuugen64 <Yuugen64@protonmail.com>
2020-10-31 19:54:12 -0500

    [004-0.0.4]: Managed to make all 3 cards flip on tap for the mobile version. However, the flip only occurs if the text area is clicked (probably due to that having specific front or back associated with it) and the flip-back animation doesn't play i.e it occurs instantly. Also, two of the three cards start with their backs presented but the first one presents the front for some reason.

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 console.log("[app.js]: Attached and working correctly!");
 
 const card1front = document.getElementById("card1front");
-const card1back = document.getElementById("card1back")
+const card1back = document.getElementById("card1back");
+const card2front = document.getElementById("card2front");
+const card2back = document.getElementById("card2back");
+const card3front = document.getElementById("card3front");
+const card3back = document.getElementById("card3back");
+
 const intro = document.getElementById("intro");
 
 let attachClickHandlers = () => {
     console.log("Handlers attached");
 
     card1front.addEventListener('click', function(){
-        console.log("Click registered!");
+        console.log("Card Front 1 - Click registered!");
         card1.classList.toggle("flip");
     });
     card1back.addEventListener('click', function(){
-        console.log("Click registered!");
+        console.log("Card Back 1 - Click registered!");
         card1.classList.toggle("flip");
+    });
+
+    card2front.addEventListener('click', function(){
+        console.log("Card Front 2 - Click registered!");
+        card2.classList.toggle("flip");
+    });
+    card2back.addEventListener('click', function(){
+        console.log("Card Back 2 - Click registered!");
+        card2.classList.toggle("flip");
+    });
+
+    card3front.addEventListener('click', function(){
+        console.log("Card Front 3 - Click registered!");
+        card3.classList.toggle("flip");
+    });
+    card3back.addEventListener('click', function(){
+        console.log("Card Back 3 - Click registered!");
+        card3.classList.toggle("flip");
     });
 
     intro.addEventListener("click", function(){
@@ -21,7 +44,7 @@ let attachClickHandlers = () => {
     });
 };
 
-if(card1 && intro){
+if(card1 && card2 && card3 && intro){
     attachClickHandlers();
 }
 
